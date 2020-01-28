3d6186806887a7aca9efba44f900c944210f38bb
W3401 <W3401@lab.aca>
2020-01-28 18:07:58 -0500

    Se implemento patron chain

--- enunciado.ts
+++ enunciado.ts
@@ -4,12 +4,63 @@ interface Proceso
     consumo : number
     nombre : string
 }
+//----------------------------------
+abstract class Nivel{
+    sucesor : Nivel
+    addSucesor(sucesor : Nivel){
+        this.sucesor = sucesor;
+    }
+    abstract matar( proceso : Proceso);
+}
+class NivelUno extends Nivel{
+    matar(proceso: Proceso) {
+        if (proceso.consumo > 100 && proceso.prioridad == 3){
+            console.log(`EjecutorNivel1 matará proceso ${proceso.nombre}`);
+        }else{
+            this.sucesor.matar(proceso);
+        }
+    }
 
+}
+class NivelDos extends Nivel{
+    matar(proceso: Proceso) {
+        if (proceso.consumo > 100 && proceso.prioridad == 2){
+            console.log(`EjecutorNivel2 matará proceso ${proceso.nombre}`);
+        }else{
+            this.sucesor.matar(proceso);
+        }
+    }
+
+}
+class NivelTres extends Nivel{
+    matar(proceso: Proceso) {
+        if (proceso.consumo > 100 && proceso.prioridad == 1){
+            console.log(`EjecutorNivel1 matará proceso ${proceso.nombre}`);
+        }else{
+            this.sucesor.matar(proceso);
+        }
+    }
+
+}
+//----------------------------------
+// clase singleton solo se instancia una vez
 class Ejecutor
 {
+    n1 : Nivel;
+    n2 : Nivel;
+    n3 : Nivel;
+    constructor(){
+            this.n1 = new NivelUno();
+            this.n2 = new NivelDos();
+            this.n3 = new NivelTres();
+
+            this.n1.addSucesor(this.n2);
+            this.n2.addSucesor(this.n3);
+    }
     matar(proceso : Proceso)
     {
-        if (proceso.prioridad == 3 && proceso.consumo > 100)
+        this.n1.matar(proceso);
+        /*if (proceso.prioridad == 3 && proceso.consumo > 100)
         {
             console.log(`EjecutorNivel1 matará proceso ${proceso.nombre}`)
         }else if (proceso.prioridad == 2 && proceso.consumo > 100)
@@ -18,7 +69,7 @@ class Ejecutor
         }else if (proceso.prioridad == 1 && proceso.consumo > 100)
         {
             console.log(`EjecutorNivel3 matará proceso ${proceso.nombre}`)
-        }
+        }*/
     }
 }
 
@@ -32,5 +83,5 @@ let main = () =>
     let en = new Ejecutor()
     en.matar(proceso)
 }
-
+ 
 main();
